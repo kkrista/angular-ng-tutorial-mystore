e4aab984fe97248c0e0ac97bce0bc4f0b76e7883
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-06 14:41:21 +0200

    created components imported to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { BackbtnComponent } from './backbtn/backbtn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ProductAlertComponent,
+    BackbtnComponent
   ],
   imports: [
     BrowserModule
