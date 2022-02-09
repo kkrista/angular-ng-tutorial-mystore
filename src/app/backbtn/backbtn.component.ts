090f76733e7f4801221b0b6130a2f53455dc95f7
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-09 12:17:46 +0200

    @fortawesome imported

--- src/app/backbtn/backbtn.component.ts
+++ src/app/backbtn/backbtn.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-backbtn',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackbtnComponent implements OnInit {
 
+  faLongArrowAltLeft = faLongArrowAltLeft;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
