1b74fbde2c77b5533f33763093a303219193f99d
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-09 12:15:32 +0200

    products and router imported to the file

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+products = products;
+  product: { name: string; price: number; short_desription: string; description: string; short_description: string; imgUrl: string; } | { name: string; price: number; short_description: string; imgUrl: string; description: string; short_desription?: undefined; } | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) {  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log(this.products);
+     this.route.paramMap.subscribe(params => {
+     this.product = products[+get('productId')];
+   });
+
+  }
+
+  addToCart(product: any) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
   }
 
 }
+function get(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
