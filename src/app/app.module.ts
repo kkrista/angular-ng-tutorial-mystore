3fd3dcaad895ca532b15dc22491f0f468b001ac6
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-09 14:12:04 +0200

    footer component imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BackbtnComponent } from './backbtn/backbtn.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { CartService } from './cart.service';
 import { BackButtonDirective } from './back-button.directive';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { BackButtonDirective } from './back-button.directive';
     ProductAlertComponent,
     BackbtnComponent,
     ShippingComponent,
-    BackButtonDirective
+    BackButtonDirective,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
